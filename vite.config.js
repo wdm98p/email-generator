0932f074fbf44e7c290d88f98136847c1f617e35
wdm98p <wes@breakroomaudio.com>
2025-07-22 13:29:53 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html'),
+    },
+    outDir: 'dist',
+  },
 })
