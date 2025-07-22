1905ecccb95479965db57b6521fafe01ed77a720
wdm98p <wes@breakroomaudio.com>
2025-07-22 13:00:49 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: 'src/main.jsx', // 👈 This is your real entry point
+    },
+  },
 });
